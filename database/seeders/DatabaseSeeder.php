1ec2a598e0d2e298dfba197ff7656b5489f8ceca
LashenDEV <lashenkari@gmail.com>
2022-04-22 20:19:55 +0530

    ✅ users seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\user::factory(10)->create();
+        $this->call(UsersSeeder::class);
     }
 }

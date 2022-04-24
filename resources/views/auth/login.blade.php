e17cbccdfb0eabff49b5dc266b29d9519b6e4f58
ChandanaEkanayaka <jagath99t@gmail.com>
2022-04-24 12:45:30 +0530

    ✅login done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,70 @@
-@extends('layouts.app')
-
+@extends('layouts.auth')
+@section('title', 'Login')
 @section('content')
-<div class="container">
+    <body class="bg-light-gray" id="body" style="background-image: url({{ asset('frontend/assets/img/auth/signin.png') }}); background-repeat: no-repeat; background-size: cover;">
+        <div class="container d-flex flex-column justify-content-between vh-100">
+            <div class="row justify-content-end mt-5">
+                <div class="col-xl-5 col-lg-6 col-md-10">
+                    <div class="card">
+                        <div class="card-header bg-primary">
+                            <div class="app-brand">
+                                
+                                
+                                <a href="/index.html">
+                                    <svg class="brand-icon" xmlns="http://www.w3.org/2000/svg"
+                                        preserveAspectRatio="xMidYMid" width="30" height="33" viewBox="0 0 30 33">
+                                        <g fill="none" fill-rule="evenodd">
+                                           
+                                        </g>
+                                    </svg>
+                                    <span class="brand-name" style="background-image: url({{ asset('frontend/assets/img/auth/teamup.png') }}); background-repeat: no-repeat; background-size:cover"> teamUp</span>
+                                    <span class="brand-name" > TeamUp</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="card-body p-5">
+
+                            <h4 class="text-dark mb-5">Sign In</h4>
+                            <form action="/index.html">
+                                <div class="row">
+                                    <div class="form-group col-md-12 mb-4">
+                                        <input type="email" class="form-control input-lg" id="email"
+                                            aria-describedby="emailHelp" placeholder="Username">
+                                    </div>
+                                    <div class="form-group col-md-12 ">
+                                        <input type="password" class="form-control input-lg" id="password"
+                                            placeholder="Password">
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="d-flex my-2 justify-content-between">
+                                            <div class="d-inline-block mr-3">
+                                                <label class="control control-checkbox">Remember me
+                                                    <input type="checkbox" />
+                                                    <div class="control-indicator"></div>
+                                                </label>
+
+                                            </div>
+                                            <p><a class="text-blue" href="#">Forgot Your Passwords?</a></p>
+                                        </div>
+                                        <button type="submit" class="btn btn-lg btn-primary btn-block mb-4">Sign In</button>
+                                        <p>Don't have an account yet ?
+                                            <a class="text-blue" href="sign-up.html">Sign Up</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="copyright pl-0">
+                <p class="text-center">&copy; 2018 Copyright Sleek Dashboard Bootstrap Template by
+                    <a class="text-primary" href="http://www.iamabdus.com/" target="_blank">Abdus</a>.
+                </p>
+            </div>
+        </div>
+    </body>
+    {{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -69,5 +132,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection

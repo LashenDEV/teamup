e3cbb3f1ae8d0296b8c9c8ee701fdf3479df6e8a
Lashen Kariyawasam <56374597+LashenDEV@users.noreply.github.com>
2022-04-25 23:23:14 +0530

    ✅ user index, profile page setuped

--- resources/views/user/Dashboard/profile.blade.php
+++ resources/views/user/Dashboard/profile.blade.php
@@ -1,25 +1,41 @@
-@extends('layouts.app')
+@extends('layouts.member')
 
 @section('content')
+<!-- ======= Breadcrumbs ======= -->
+<section id="breadcrumbs" class="breadcrumbs">
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('dashboard') }}</div>
-                    <h4>Hi User: {{ Auth::user()->name }}</h4>
-                    <hr>
-                    PROFILE
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
 
-                        {{ __('You are logged in!') }}
+      <ol>
+        <li><a href="{{ route('user.dashboard') }}">Home</a></li>
+      </ol>
+      <h2>Profile</h2>
+
+    </div>
+  </section><!-- End Breadcrumbs -->
+    <div class="container">
+      <div class="row">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('dashboard') }}</div>
+                        <h4>Hi User: {{ Auth::user()->name }}</h4>
+                        <hr>
+                        PROFILE
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+    
+                            {{ __('You are logged in!') }}
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+      </div>
     </div>
+  </section>
 @endsection

fcca6cca8bbcc1275d2084b41a5b8c690b7bf508
ChandanaEkanayaka <jagath99t@gmail.com>
2022-04-29 21:37:31 +0530

    ✅ register view done

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,69 @@
-@extends('layouts.app')
+@extends('layouts.auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+    <body class="bg-light-gray" id="body"
+        style="background-image: url({{ asset('frontend/assets/img/auth/singup.jpg') }}); background-repeat: no-repeat; background-size: cover;">
+        >
+        <div class="container d-flex flex-column justify-content-between vh-100">
+            <div class="row justify-content-end mt-5">
+                <div class="col-xl-5 col-lg-6 col-md-10">
+                    <div class="card border-0">
+                        <div class="card-header bg-primary">
+                            <div class="app-brand">
+                                <a href="/index.html" class="pl-0">
+                                    <img src="{{ asset('logos/teamup logo.png') }}" alt="">
+                                    <span class="brand-name">
+                                        <h1>Teamup</h1>
                                     </span>
-                                @enderror
-                            </div>
+                                </a>
+                            </div>f
                         </div>
+                        <div class="card-body p-5">
+                            <h4 class="text-dark mb-5">Sign Up</h4>
+                            <form action="/index.html">
+                                <div class="row">
+                                    <div class="form-group col-md-12 mb-4">
+                                        <input type="text" class="form-control input-lg" id="name"
+                                            aria-describedby="nameHelp" placeholder="Name">
+                                    </div>
+                                    <div class="form-group col-md-12 mb-4">
+                                        <input type="email" class="form-control input-lg" id="email"
+                                            aria-describedby="emailHelp" placeholder="Username">
+                                    </div>
+                                    <div class="form-group col-md-12 ">
+                                        <input type="password" class="form-control input-lg" id="password"
+                                            placeholder="Password">
+                                    </div>
+                                    <div class="form-group col-md-12 ">
+                                        <input type="password" class="form-control input-lg" id="cpassword"
+                                            placeholder="Confirm Password">
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="d-inline-block mr-3">
+                                            <label class="control control-checkbox">
+                                                <input type="checkbox" />
+                                                <div class="control-indicator"></div>
+                                                I Agree the terms and conditions
+                                            </label>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                                        </div>
+                                        <button type="submit" class="btn btn-lg btn-primary btn-block mb-4">Sign Up</button>
+                                        <p>Already have an account?
+                                            <a class="text-blue" href="sign-in.html">Sign in</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </form>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
+            <div class="copyright pl-0">
+                <p class="text-center">&copy; 2022 Copyright by @TeamUp
+                </p>
+            </div>
         </div>
-    </div>
-</div>
+    </body>
 @endsection

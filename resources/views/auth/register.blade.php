24c50ca6f9679ed494b35634ba32ea856c84457b
Lashen Kariyawasam <56374597+LashenDEV@users.noreply.github.com>
2022-05-02 17:40:46 +0530

    ✅ register page backend linked

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <body class="bg-light-gray" id="body"
-        style="background-image: url({{ asset('frontend/assets/img/auth/singup.jpg') }}); background-repeat: no-repeat; background-size: cover;">
+        style="background-image: url({{ asset('assets/images/auth/singup.jpg') }}); background-repeat: no-repeat; background-size: cover;">
         >
         <div class="container d-flex flex-column justify-content-between vh-100">
             <div class="row justify-content-end mt-5">
@@ -12,7 +12,7 @@
                         <div class="card-header bg-primary">
                             <div class="app-brand">
                                 <a href="/index.html" class="pl-0">
-                                    <img src="{{ asset('logos/teamup logo.png') }}" alt="">
+                                    <img src="{{ asset('assets/images/logos/teamup logo.png') }}" alt="">
                                     <span class="brand-name">
                                         <h1>Teamup</h1>
                                     </span>
@@ -21,41 +21,58 @@
                         </div>
                         <div class="card-body p-5">
                             <h4 class="text-dark mb-5">Sign Up</h4>
-                            <form action="/index.html">
+                            <form method="POST" action="{{ route('register') }}">
+                                @csrf
                                 <div class="row">
                                     <div class="form-group col-md-12 mb-4">
-                                        <input type="text" class="form-control input-lg" id="name"
-                                            aria-describedby="nameHelp" placeholder="Name">
+                                        <input type="text"
+                                            class="form-control input-lg  @error('name') is-invalid @enderror" id="name"
+                                            aria-describedby="nameHelp" placeholder="Name" name="name"
+                                            value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                        @error('name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
                                     </div>
                                     <div class="form-group col-md-12 mb-4">
-                                        <input type="email" class="form-control input-lg" id="email"
-                                            aria-describedby="emailHelp" placeholder="Username">
+                                        <input type="email"
+                                            class="form-control input-lg @error('email') is-invalid @enderror" id="email"
+                                            aria-describedby="emailHelp" placeholder="Email" name="email"
+                                            value="{{ old('email') }}" required autocomplete="email">
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
                                     </div>
                                     <div class="form-group col-md-12 ">
-                                        <input type="password" class="form-control input-lg" id="password"
+                                        <input type="password"
+                                            class="form-control input-lg @error('password') is-invalid @enderror"
+                                            name="password" required autocomplete="new-password" id="password"
                                             placeholder="Password">
+
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
                                     </div>
                                     <div class="form-group col-md-12 ">
-                                        <input type="password" class="form-control input-lg" id="cpassword"
-                                            placeholder="Confirm Password">
+                                        <input type="password" class="form-control input-lg" id="password-confirm"
+                                            placeholder="Confirm Password" name="password_confirmation" required
+                                            autocomplete="new-password">
                                     </div>
                                     <div class="col-md-12">
                                         <div class="d-inline-block mr-3">
-                                            <label class="control control-checkbox">
-                                                <input type="checkbox" />
-                                                <div class="control-indicator"></div>
-                                                I Agree the terms and conditions
-                                            </label>
-
                                         </div>
                                         <button type="submit" class="btn btn-lg btn-primary btn-block mb-4">Sign Up</button>
                                         <p>Already have an account?
-                                            <a class="text-blue" href="sign-in.html">Sign in</a>
+                                            <a class="text-blue" href="{{ route('login') }}">Sign in</a>
                                         </p>
                                     </div>
                                 </div>
                             </form>
-
                         </div>
                     </div>
                 </div>

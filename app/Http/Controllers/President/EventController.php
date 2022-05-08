efda96331fbcad0bdab253c36e9486f9f1f09703
Lashen Kariyawasam <56374597+LashenDEV@users.noreply.github.com>
2022-05-08 20:45:04 +0530

    ✅ event layout for development

---
+++ app/Http/Controllers/President/EventController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\President;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Laravel\Ui\Presets\React;
+
+class EventController extends Controller
+{
+    public function index()
+    {
+        return view('president.event.index');
+    }
+
+    public function create()
+    {
+        return view('president.event.new');
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function edit($id)
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
+    }
+
+    public function destroy($id)
+    {
+        return redirect()->back()->with('success', 'Event Deleted Successfully');
+    }
+}

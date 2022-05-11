873612be88a0abf917616619cae755121b06d4b5
Lashen Kariyawasam <56374597+LashenDEV@users.noreply.github.com>
2022-05-11 20:29:27 +0530

    ✅ backend connected

---
+++ database/migrations/2022_05_11_125752_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('president_id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('image');
+            $table->date('date');
+            $table->time('time');
+            $table->string('venue');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}

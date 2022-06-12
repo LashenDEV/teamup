2673672350ebced5641542259c1dc4b26f5dbbec
ChandanaEkanayaka <jagath99t@gmail.com>
2022-06-12 09:33:14 +0530

    :construction: club categories view

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -122,7 +122,7 @@
                                 <td bgcolor="#ffffff" align="center" valign="top"
                                     style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
                                     <h1 style="font-size: 48px; font-weight: 400; margin: 2;">Welcome To TeamUp !</h1> <img
-                                        src=" {{ https://img.icons8.com/clouds/100/000000/handshake.png }}" width="125"
+                                        src="https://img.icons8.com/clouds/100/000000/handshake.png" width="125"
                                         height="120" style="display: block; border: 0px;" />
                                 </td>
                             </tr>

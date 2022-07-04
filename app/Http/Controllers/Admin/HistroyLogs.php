069ee31b445ce540780d9eeed5473a12f59341e1
Lashen Kariyawasam <56374597+LashenDEV@users.noreply.github.com>
2022-07-05 02:03:57 +0530

    ✅ history logging done

--- app/Http/Controllers/Admin/HistroyLogs.php
+++ app/Http/Controllers/Admin/HistroyLogs.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\HistoryLogs;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HistroyLogs extends Controller
 {
     public function index(){
-        return view('admin.histroy-logs.index');
+        $history_logs = HistoryLogs::where('user_id', Auth::user()->id)->paginate(5);
+        return view('admin.histroy-logs.index', compact('history_logs'));
     }
 }
--- app/Http/Controllers/President/HistroyLogs.php
+++ app/Http/Controllers/President/HistroyLogs.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\President;
 
 use App\Http\Controllers\Controller;
+use App\Models\HistoryLogs;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HistroyLogs extends Controller
 {
-    public function index(){
-        return view('president.histroy-logs.index');
+    public function index()
+    {
+        $history_logs = HistoryLogs::where('user_id', Auth::user()->id)->paginate(5);
+        return view('president.histroy-logs.index', compact('history_logs'));
     }
 }

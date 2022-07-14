12b39462840c59d3b1af783af3552a36cf769ad7
ChandanaEkanayaka <jagath99t@gmail.com>
2022-07-14 19:20:45 +0530

    ✅ president dashboard done

--- resources/views/president/dashboard/index.blade.php
+++ resources/views/president/dashboard/index.blade.php
@@ -1,25 +1,185 @@
 @extends('layouts.president')
 @section('title', 'dashboard')
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('dashboard') }}</div>
-                    <h4>Hi President: {{ Auth::user()->name }}</h4>
-                    <hr>
-                    DASHBOARD 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
+<div class="py-12 px-3 ">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="container-fluid">
+            <div class="d-sm-flex align-items-center justify-content-between mt-4 mb-4">
+                <h2>President Dashboard</h2>
+                <a href="/">
+                    <button class="btn btn-outline-secondary">view site</button>
+                </a>
+            </div>
+            <div class="row mt-4">
+                <div class="col-xl-3 col-sm-6">
+                    <div class="card card-mini mb-4 shadow bg-body rounded">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-around">
+                                <div class="p-4">
+                                    <i class="fa-duotone fa-user-tie fa-5x text-primary"></i>
+                                </div>
+                                <div class="text-center d-flex flex-column justify-content-center">
+                                    <p class="mb-1">Total President Count</p>
+                                </div>
                             </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-6">
+                    <div class="card card-mini mb-4 shadow bg-body rounded">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-around">
+                                <div class="p-4">
+                                    <i class="fa-duotone fa-users-line fa-5x text-warning"></i>
+                                </div>
+                                <div class="text-center d-flex flex-column justify-content-center">
+                                    <p class="mb-1">Total Members Count</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-6">
+                    <div class="card card-mini mb-4 shadow bg-body rounded">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-around">
+                                <div class="p-4">
+                                    <i class="fa-duotone fa-object-exclude fa-5x text-success"></i>
+                                </div>
+                                <div class="text-center d-flex flex-column justify-content-center">
+                                    <p class="mb-1">Total Clubs Count</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-6">
+                    <div class="card card-mini mb-4 shadow bg-body rounded">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-around">
+                                <div class="p-4">
+                                    <i class="fa-duotone fa-user-tie fa-5x"></i>
+                                </div>
+                                <div class="text-center d-flex flex-column justify-content-center">
+                                    <p class="mb-1">New Visitors Today</p>
+                                    <h1>9,503</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-xl-8 col-md-12">
+                    <!-- Registration Graph -->
+                    <div class="card card-default shadow bg-body rounded" data-scroll-height="675">
+                        <div class="card-header">
+                            <h2>Overview of registration for the year</h2>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="linechart" class="chartjs"></canvas>
+                        </div>
+                        <div class="card-footer d-flex flex-wrap bg-white p-0">
+                            <div class="col-6 px-0">
+                                <div class="text-center p-4">
+                                    <h4>6,308</h4>
+                                    <p class="mt-2">Total clubs registered in the year</p>
+                                </div>
+                            </div>
+                            <div class="col-6 px-0">
+                                <div class="text-center p-4 border-left">
+                                    <h4>70,506</h4>
+                                    <p class="mt-2">Total members registered in the year</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-12">
+                    <!-- Doughnut Chart -->
+                    <div class="card card-default shadow bg-body rounded" data-scroll-height="675">
+                        <div class="card-header justify-content-center">
+                            <h2>Most Engaged Clubs</h2>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="doChart"></canvas>
+                        </div>
+                        <a href="#" class="pb-5 d-block text-center text-muted"><i
+                                class="mdi mdi-download mr-2"></i> Download overall report</a>
+                        <div class="card-footer d-flex flex-wrap bg-white p-0">
+                            <div class="col-6">
+                                <div class="py-4 px-4">
+                                    <ul class="d-flex flex-column justify-content-between">
+                                        <li class="mb-2"><i class="mdi mdi-checkbox-blank-circle-outline mr-2"
+                                                            style="color: #4c84ff"></i>Rotract Club
+                                        </li>
+                                        <li><i class="mdi mdi-checkbox-blank-circle-outline mr-2"
+                                               style="color: #80e1c1 "></i>Leo Club
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-6 border-left">
+                                <div class="py-4 px-4 ">
+                                    <ul class="d-flex flex-column justify-content-between">
+                                        <li class="mb-2"><i class="mdi mdi-checkbox-blank-circle-outline mr-2"
+                                                            style="color: #8061ef"></i>Art Club
+                                        </li>
+                                        <li><i class="mdi mdi-checkbox-blank-circle-outline mr-2"
+                                               style="color: #ffa128"></i>English Club
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-12">
+                    <div class="card card-table-border-none shadow bg-body rounded" id="recent-orders">
+                        <div class="card-header justify-content-between">
+                            <h2>Recent Club Status</h2>
+                        </div>
+                        <div class="card-body pt-0 pb-5">
+                            <table class="table card-table table-responsive table-responsive-large"
+                            style="width:100%">
+                         <thead>
+                         <tr>
+                             <th>ID</th>
+                             <th>Name</th>
+                             <th class="d-none d-md-table-cell">Created Date</th>
+                             <th class="d-none d-md-table-cell">Category</th>
+                             <th>Status</th>
+                             <th>Actions</th>
+                         </tr>
+                         </thead>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td><span class="badge badge-success">Approved</span>                          
+                                    <td class="d-none d-md-table-cell">
+                                                <button type="button" class="btn btn-success"><i
+                                                        class="fa-duotone fa-circle-check"></i>
+                                                    Approve
+                                                </button>
+                                    <td class="d-none d-md-table-cell">
+                                            <a class="pl-1">
+                                                <button type="submit" class="btn btn-danger"><i
+                                                        class="fa-duotone fa-circle-xmark"></i>
+                                                    Reject
+                                                </button>
+                                            </a>
+                                     </td>
+                                  </tr>
+                              </tbody>
+                          </table>
+                       </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection

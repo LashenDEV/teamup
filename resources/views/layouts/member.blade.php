0c520e6a22bb8e106270d549437bc40290c231c1
Madurangi Jayathissa <92798397+MadurangiJayathissa@users.noreply.github.com>
2022-07-18 00:23:18 +0530

    💄footer redesigned

--- resources/views/layouts/member.blade.php
+++ resources/views/layouts/member.blade.php
@@ -75,12 +75,14 @@
 <body>
 
 <!-- ======= Header ======= -->
-<header id="header" class="fixed-top d-flex align-items-center ">
+<header id="header" class="fixed-top d-flex align-items-center" style="height:80px;">
     <div class="container-fluid">
-
         <div class="row justify-content-center align-items-center">
             <div class="col-xl-11 d-flex align-items-center justify-content-between">
+                <div class="d-flex align-items-center justify-content-between">
+                <img src="{{ asset('logos/teamup-logo-new.png') }}" alt="logo" style="width:50px;" class="img-fluid"> &nbsp &nbsp &nbsp
                 <h1 class="logo"><a href="{{ route('user.dashboard') }}">TeamUp</a></h1>
+                </div>
                 <!-- Uncomment below if you prefer to use an image logo -->
                 <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
@@ -184,62 +186,52 @@
     <div class="footer-top">
         <div class="container">
             <div class="row">
-
-                <div class="col-lg-3 col-md-6 footer-info">
-                    <h3>Teamup</h3>
-                    <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna
-                        derita valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque
-                        felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
+            <div class="d-flex justify-content-between flex-md-row flex-column">
+                <div class="col-lg-2 col-md-4 footer-logo d-flex align-items-center justify-content-center pb-5 me-md-5">
+                    <img src="{{ asset('logos/teamup-logo-new.png') }}" alt="logo" style="width:300px;" class="img-fluid">
                 </div>
 
-                <div class="col-lg-3 col-md-6 footer-links">
+                <div class="col-lg-3 col-md-9 footer-info">
+                    <h3>Teamup</h3>
+                    <p>We are pleased to welcome you to the University club management system. We have an excellent reputation for creating 
+                        innovative entrepreneurs. We aim to give every student in our care the best possible education to prepare them for
+                        life beyond University. Through this system, you can view any club of the University and stay connected with us.</p>
+                </div>
+
+                <div class="col-lg-3 col-md-3 footer-links d-flex flex-column">
                     <h4>Useful Links</h4>
                     <ul>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">About us</a></li>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">Services</a></li>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">Terms of service</a></li>
-                        <li><i class="bi bi-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#hero">Home</a></li>
+                        <li><i class="bi bi-chevron-right"></i> <a href="#portfolio">Clubs</a></li>
                     </ul>
                 </div>
 
                 <div class="col-lg-3 col-md-6 footer-contact">
                     <h4>Contact Us</h4>
                     <p>
-                        A108 Adam Street <br>
-                        New York, NY 535022<br>
-                        United States <br>
-                        <strong>Phone:</strong> +1 5589 55488 55<br>
-                        <strong>Email:</strong> info@example.com<br>
+                        105 Passara Road,<br>
+                            Badulla,<br>
+                            Sri Lanka. <br>
+                            <strong>Phone:</strong> +94 335672910<br>
+                            <strong>Email:</strong> teamup@gmail.com <br>
                     </p>
 
                     <div class="social-links">
                         <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
                         <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
                         <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
                         <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
                     </div>
 
                 </div>
-
-                <div class="col-lg-3 col-md-6 footer-newsletter">
-                    <h4>Our Newsletter</h4>
-                    <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam
-                        illum dolore legam minim quorum culpa amet magna export quem marada parida nodela
-                        caramase seza.</p>
-                    <form action="" method="post">
-                        <input type="email" name="email"><input type="submit" value="Subscribe">
-                    </form>
-                </div>
-
+            </div>
             </div>
         </div>
     </div>
 
     <div class="container">
         <div class="copyright">
-            &copy; Copyright <strong>BizPage</strong>. All Rights Reserved
+            &copy; Copyright <strong>TeamUp</strong>. All Rights Reserved
         </div>
         <div class="credits">
             <!--
@@ -248,7 +240,7 @@ You can delete the links only if you purchased the pro version.
 Licensing information: https://bootstrapmade.com/license/
 Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=BizPage
 -->
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            Designed by <a href="https://bootstrapmade.com/">#team12</a>
         </div>
     </div>
 </footer><!-- End Footer -->

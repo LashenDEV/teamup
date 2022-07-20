50a611f758d66a9f79036f1f912122f6685f770d
Madurangi Jayathissa <92798397+MadurangiJayathissa@users.noreply.github.com>
2022-07-20 18:04:22 +0530

    ✅ services add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,8 +69,8 @@
         <div class="row justify-content-center align-items-center">
             <div class="col-xl-11 d-flex align-items-center justify-content-between">
                 <div class="d-flex align-items-center justify-content-between">
-                    <img src="{{ asset('logos/teamup-logo-new.png') }}" alt="logo" style="width:50px;"
-                         class="img-fluid"> &nbsp &nbsp &nbsp
+                    <a href="/"><img src="{{ asset('logos/teamup-logo-new.png') }}" alt="logo" style="width:50px;"
+                         class="img-fluid"></a> &nbsp &nbsp &nbsp
                     <h1 class="logo"><a href="/">TeamUp</a></h1>
                 </div>
 
@@ -273,54 +273,42 @@
 
             <header class="section-header wow fadeInUp">
                 <h3>Services</h3>
-                <p>Laudem latine persequeris id sed, ex fabulas delectus quo. No vel partiendo abhorreant
-                    vituperatoribus, ad pro quaestio laboramus. Ei ubique vivendum pro. At ius nisl accusam lorenta
-                    zanos paradigno tridexa panatarel.</p>
+                <p></p>
             </header>
 
             <div class="row">
 
                 <div class="col-lg-4 col-md-6 box" data-aos="fade-up" data-aos-delay="100">
-                    <div class="icon"><i class="bi bi-briefcase"></i></div>
-                    <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-                    <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias
-                        excepturi
-                        sint occaecati cupiditate non provident</p>
+                    <div class="icon"><i class="bi bi-pencil-square"></i></div>
+                    <h4 class="title"><a href="">Registration Services</a></h4>
+                    <p class="description">Any student of Uva Welassa University can register in TeamUp online.</p>
                 </div>
                 <div class="col-lg-4 col-md-6 box" data-aos="fade-up" data-aos-delay="200">
-                    <div class="icon"><i class="bi bi-card-checklist"></i></div>
-                    <h4 class="title"><a href="">Dolor Sitema</a></h4>
-                    <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea
-                        commodo consequat tarad limino ata</p>
+                    <div class="icon"><i class="bi bi-coin"></i></div>
+                    <h4 class="title"><a href="">Paying Services</a></h4>
+                    <p class="description">Payments can be made online through PayPal while registering.</p>
                 </div>
                 <div class="col-lg-4 col-md-6 box" data-aos="fade-up" data-aos-delay="300">
-                    <div class="icon"><i class="bi bi-bar-chart"></i></div>
-                    <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-                    <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                        dolore
-                        eu fugiat nulla pariatur</p>
+                    <div class="icon"><i class="bi bi-calendar2-date"></i></div>
+                    <h4 class="title"><a href="">Meeting</a></h4>
+                    <p class="description">Conducting meetings through an online zoom meeting tool.</p>
                 </div>
                 <div class="col-lg-4 col-md-6 box" data-aos="fade-up" data-aos-delay="200">
-                    <div class="icon"><i class="bi bi-binoculars"></i></div>
-                    <h4 class="title"><a href="">Magni Dolores</a></h4>
-                    <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                        officia
-                        deserunt mollit anim id est laborum</p>
+                    <div class="icon"><i class="bi bi-award"></i></div>
+                    <h4 class="title"><a href="">Awarding</a></h4>
+                    <p class="description">E-certificates are issued for the tournaments conducted through TeamUp.</p>
                 </div>
                 <div class="col-lg-4 col-md-6 box" data-aos="fade-up" data-aos-delay="300">
                     <div class="icon"><i class="bi bi-brightness-high"></i></div>
-                    <h4 class="title"><a href="">Nemo Enim</a></h4>
-                    <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui
-                        blanditiis
-                        praesentium voluptatum deleniti atque</p>
+                    <h4 class="title"><a href="">Club Activities</a></h4>
+                    <p class="description">TeamUp is a very supportive environment for the co-curricular activities of students. They are encouraged to get 
+                        involved in the organizing of social activities, charity work, and activities pertaining to their own personality development</p>
                 </div>
                 <div class="col-lg-4 col-md-6 box" data-aos="fade-up" data-aos-delay="400">
-                    <div class="icon"><i class="bi bi-calendar4-week"></i></div>
-                    <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
-                    <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero
-                        tempore,
-                        cum soluta nobis est eligendi</p>
+                    <div class="icon"><i class="bi bi-building"></i></div>
+                    <h4 class="title"><a href="">Recreational Facilities</a></h4>
+                    <p class="description">TeamUp recreational building consists of a swimming pool with 6 lanes, a multi-purpose indoor sports building 
+                        including a basketball court, a badminton court, and a table tennis court./p>
                 </div>
 
             </div>
